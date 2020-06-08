4b1506de69ae5f0aee9b13d3333e7e9f7bd33c03
usrbinkat <usrbinkat@braincraft.io>
2020-06-08 15:38:38 +0000

    working towards automated kilo+subspace

--- terraform/gcp/instance/ensign.tf
+++ terraform/gcp/instance/ensign.tf
@@ -7,25 +7,22 @@ resource "google_compute_instance" "ensign" {
   machine_type    = var.ensign_type
   can_ip_forward  = true
   tags                    = ["ensign", "ministackdev", "ssh-service-public"]
-
   boot_disk {
     initialize_params {
       image = var.instance_os
+      size    = 32
     }
   }
-
   network_interface {
     network = "default"
     access_config {
       nat_ip = google_compute_address.primary_public_ipv4.address
     }
   }
-
   scheduling {
     automatic_restart   = true
     on_host_maintenance = "MIGRATE"
   }
-
   lifecycle {
     ignore_changes = [attached_disk]
   }
@@ -54,14 +51,3 @@ output "public_ipv4" {
   value = google_compute_instance.ensign.network_interface.0.access_config.0.nat_ip
 }
 
-#resource "google_compute_disk" "volume_ensign_sda" {
-#  name    = "volume_ensign_sda"
-#  type    = "pd-standard"
-#  zone    = var.region
-#  size    = var.size_volume_lxd
-#  project = var.project_id
-#}
-#resource "google_compute_attached_disk" "ensign" {
-#  disk     = google_compute_disk.volume_ensign_sda.id
-#  instance = google_compute_instance.ensign.id
-#}
